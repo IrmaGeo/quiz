74d00583de64e77cf820eb9f9d6af30399aff59b
Irma Georgia <irmamodzg@gmail.com>
2020-07-10 04:02:50 -0400

    revrite code

--- script.js
+++ script.js
@@ -1,207 +1,161 @@
-// DOM elements
-var nextBtnEL = document.getElementById("next");
-var prevBtnEL = document.getElementById("previous");
-var submitBtnEL = document.querySelector(".submit");
+var startBtnEl = document.querySelector("#startBtn");
+var nextQuestionEl = document.querySelector("#nextQuestion");
+var questionEl = document.querySelector(".question");
+var answerEl = document.getElementById("answersBtn");
+var nextbtnEl = document.getElementById("next");
+var submitEl = document.getElementById("submitQuiz");
+var prevbtnEl = document.getElementById("previous");
 var timeEl = document.querySelector(".timer");
-var startEl = document.querySelector(".start");
-var instEL = document.querySelector(".inst");
-var questionEL = document.querySelector(".question");
-var answer1EL = document.querySelector("#a1");
-var answer2EL = document.querySelector("#a2");
-var answer3EL = document.querySelector("#a3");
-var answer4EL = document.querySelector("#a4");
-var userEL = document.querySelector(".textLable");
-var nameEL = document.querySelector("#UserName");
-var scoreEL = document.querySelector(".score");
-var gobackEl = document.getElementById("goback");
-var clearEL = document.getElementById("clear");
-var resultEL = document.querySelector(".result");
-var score = 0;
-var textresultEl = document.querySelector(".textresult");
-var divTimeEl = document.querySelector(".divTime");
-console.log(divTimeEl);
-// show - hide elements
+var correctAnswers = [1, 2, 3];
+var secondsLeft = 10;
 
-startEl.addEventListener("click", hideElements);
-
-function hideElements() {
-  startEl.style.display = "none";
-  instEL.style.display = "none";
-  timeEl.style.display = "block";
-  divTimeEl.style.display = "block";
-  i = 0;
-  var secondsLeft = 10;
+// start quiz
+startBtnEl.addEventListener("click", startQuiz);
+function startQuiz() {
+  scoredivEl.classList.add("hide");
+  submitEl.classList.add("hide");
   setTime(secondsLeft);
-
-  pushQuestion(i);
+  i = 0;
+  // timer();
+  showNextQuestion(i);
+  var startQuizEl = document.getElementById("startQuiz").classList.add("hide");
+  startBtnEl.classList.add("hide");
+  nextQuestionEl.classList.remove("hide");
 }
 
-// create objects for questions
-var questions = [
-  {
-    q: "quesrion1",
-    a1: "a",
-    a2: "b",
-    a3: "c",
-    a4: "d",
-    correctAnswer: "a3",
-  },
+function showNextQuestion() {
+  resetBtn();
+  if (i <= -1) {
+    i = 0;
+  }
+  if (i <= questions.length - 1) {
+    questionEl.textContent = questions[i].question;
+    questions[i].answers.forEach((answers) => {
+      var button = document.createElement("button");
+      button.textContent = answers.text;
+      answerEl.appendChild(button);
+      if (answers.correct) {
+        // create correct answers array
+      }
+      button.addEventListener("onclick", correctAnswer);
+    });
+  } else {
+    finishQuiz();
+  }
+}
 
-  {
-    q: "There are questui2 days in a year.",
-    a1: "e",
-    a2: "f",
-    a3: "j",
-    a4: "h",
-    correctAnswer: "a2",
-  },
-  {
-    q: "There are question3 days in a year.",
-    a1: "i",
-    a2: "j",
-    a3: "k",
-    a4: "l",
-    correctAnswer: "a1",
-  },
-];
+function correctAnswer(e) {
+  // checked if this button is in correct answers array
+}
 
-//create timer function
+function setStatusClass(element, correct) {}
+
+nextbtnEl.addEventListener("click", nextQuiestion);
+function nextQuiestion() {
+  i++;
+  resetBtn();
+  showNextQuestion(i);
+}
+function resetBtn() {
+  while (answerEl.firstChild) {
+    answerEl.removeChild(answerEl.firstChild);
+  }
+}
+prevbtnEl.addEventListener("click", prevQuiestion);
+function prevQuiestion() {
+  i--;
+  resetBtn();
+  showNextQuestion(i);
+}
+function resetBtn() {
+  while (answerEl.firstChild) {
+    answerEl.removeChild(answerEl.firstChild);
+  }
+}
+
+// start TimeRanges======
+
+function finishQuiz() {
+  secondsLeft = 0;
+  nextQuestionEl.classList.add("hide");
+  submitEl.classList.remove("hide");
+  scoredivEl.classList.add("hide");
+}
+
+var submitBtnEl = document.querySelector(".submit");
+submitBtnEl.addEventListener("click", submitQuiz);
+
+var scoredivEl = document.getElementById("scoreQuiz");
+
+console.log(scoredivEl);
+
+var scoreEl = document.querySelector(".score");
+
+function submitQuiz() {
+  secondsLeft = 0;
+  submitEl.classList.add("hide");
+  scoreEl.textContent = "Your score is" + " " + correctAnswers.length;
+  console.log(scoredivEl);
+  scoredivEl.classList.remove("hide");
+}
+// // click next question======
+// push next question
+// hide start Element
+
+// // click answers=======
+// check answers and push/remove/update answers string
+
 function setTime(secondsLeft) {
+  var secondsLeft = 10;
   timeEl.textContent = secondsLeft;
 
   var timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = secondsLeft;
 
-    if (secondsLeft <= 0) {
+    if (secondsLeft <= 0 {
       clearInterval(timerInterval);
+      finishQuiz();
     }
   }, 1000);
 }
 
-//handled functions
+// // submit--> calculate======
+// calculate correct answers
+// // clear
+// clear answers string
 
-// // show questions
+// goback=======
+var goBackBtnEl = document
+  .getElementById("goback")
+  .addEventListener("click", startQuiz);
 
-function pushQuestion() {
-  var output = [];
-  var answer;
-  questionEL.innerHTML = questions[i].q;
-  questionEL.style.display = "block";
-  answer1EL.style.display = "block";
-  answer2EL.style.display = "block";
-  answer3EL.style.display = "block";
-  answer4EL.style.display = "block";
-  nextBtnEL.style.display = "block";
-  prevBtnEL.style.display = "block";
-  answer1EL.textContent = questions[i].a1;
-  answer2EL.textContent = questions[i].a2;
-  answer3EL.textContent = questions[i].a3;
-  answer4EL.textContent = questions[i].a4;
-}
-
-nextBtnEL.onclick = function () {
-  nextquestion();
-};
-function nextquestion() {
-  textresultEl.textContent = "";
-  if (i < questions.length - 1) {
-    i++;
-    pushQuestion(i);
-  } else {
-    i = questions.length - 1;
-    questionEL.style.display = "none";
-    answer1EL.style.display = "none";
-    answer2EL.style.display = "none";
-    answer3EL.style.display = "none";
-    answer4EL.style.display = "none";
-    nextBtnEL.style.display = "none";
-    prevBtnEL.style.display = "none";
-    submitBtnEL.style.display = "none";
-    userEL.style.display = "block";
-    nameEL.style.display = "block";
-    submitBtnEL.style.display = "block";
-    textresultEl.style.display = "none";
-  }
-}
-
-prevBtnEL.onclick = function () {
-  prevQuestion();
-};
-function prevQuestion() {
-  textresultEl.textContent = "";
-  if (i > 0) {
-    i--;
-    pushQuestion(i);
-  }
-}
-
-submitBtnEL.onclick = function () {
-  calculateResult();
-  secondsLeft = 0;
-};
-
-function calculateResult() {
-  scoreEL.innerHTML = "Your score is" + " " + score;
-  scoreEL.style.display = "block";
-  gobackEl.style.display = "block";
-  clearEL.style.display = "block";
-  startEl.style.display = "none";
-  userEL.style.display = "none";
-  nameEL.style.display = "none";
-  textresultEl.style.display = "none";
-  submitBtnEL.style.display = "none";
-  nameEL.innerHTML = "";
-}
-gobackEl.onclick = function () {
-  goback();
-};
-function goback() {
-  startEl.style.display = "block";
-  scoreEL.style.display = "none";
-  gobackEl.style.display = "none";
-  clearEL.style.display = "none";
-}
-
-answer1EL.onclick = function () {
-  nextBtnEL.style.display = "block";
-  prevBtnEL.style.display = "block";
-  var x = answer1EL;
-  checkAnswer(x);
-};
-function checkAnswer(x) {
-  if (x.id === questions[i].correctAnswer) {
-    score = score + 1;
-    textresultEl.textContent = "correct!";
-    textresultEl.style.display = "block";
-
-    console.log(score + " score");
-  } else {
-    textresultEl.textContent = "wrong!";
-    console.log(score);
-    textresultEl.style.display = "block";
-  }
-}
-answer2EL.onclick = function () {
-  nextBtnEL.style.display = "block";
-  prevBtnEL.style.display = "block";
-  var x = answer1EL;
-  var x = answer2EL;
-  checkAnswer(x);
-};
-
-answer3EL.onclick = function () {
-  nextBtnEL.style.display = "block";
-  prevBtnEL.style.display = "block";
-  var x = answer1EL;
-  var x = answer3EL;
-  checkAnswer(x);
-};
-
-answer4EL.onclick = function () {
-  nextBtnEL.style.display = "block";
-  prevBtnEL.style.display = "block";
-  var x = answer1EL;
-  var x = answer4EL;
-  checkAnswer(x);
-};
+var questions = [
+  {
+    question: "what is your name?",
+    answers: [
+      { text: "nino", correct: true },
+      { text: "koba", correct: false },
+      { text: "giorgi", correct: false },
+      { text: "tamari", correct: false },
+    ],
+  },
+  {
+    question: "what is your last name?",
+    answers: [
+      { text: "giorgadze", correct: false },
+      { text: "levanidze", correct: true },
+      { text: "kobaidze", correct: false },
+      { text: "shubitidze", correct: false },
+    ],
+  },
+  {
+    question: "what is your favorite color?",
+    answers: [
+      { text: "blue", correct: false },
+      { text: "yello", correct: false },
+      { text: "green", correct: true },
+      { text: "purple", correct: false },
+    ],
+  },
+];
